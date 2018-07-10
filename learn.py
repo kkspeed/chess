6e0553f48fab455844954ffd50103a8754788e28
Unknown <leilmyxwz@gmail.com>
2018-07-09 23:08:17 -0700

    Add encoder

--- learn.py
+++ learn.py
@@ -1,1 +1,58 @@
 from chess_types import *
+
+import copy
+import collections
+import numpy as np
+
+BOARD = ['帅', '将', '士', '仕', '相', '象', '马', '馬', '车', '車', '兵', '卒', '炮', '包']
+
+DEFAULT = {
+    '帅': [-1],
+    '将': [-1],
+    '士': [-1, -1],
+    '仕': [-1, -1],
+    '相': [-1, -1],
+    '象': [-1, -1],
+    '马': [-1, -1],
+    '馬': [-1, -1],
+    '车': [-1, -1],
+    '車': [-1, -1],
+    '兵': [-1, -1, -1, -1, -1],
+   	'卒': [-1, -1, -1, -1, -1],
+   	'炮': [-1, -1],
+   	'包': [-1, -1]
+}
+
+
+class SimpleEncoder:
+	def encode(self, state):
+		d = copy.deepcopy(DEFAULT)
+		for p in state.board.pieces:
+			name = str(p)
+			i = d[name].index(-1)
+			d[name][i] = self.point_to_index(p.pos)
+			d[name].sort()
+		result = []
+		for key in BOARD:
+			result.extend(d[key])
+		result.append(1 if state.player == Player.red else 0)
+		result.append(state.steps)
+		return np.array(result)
+
+	def decode(self, array) -> GameState:
+		board = Board()
+		for index, elem in enumerate(array[:-2]):
+			if array[index] >= 0:
+				name = BOARD[index]
+				col = array[index] % 9
+				row = array[index] // 9
+				piece = Piece.from_name(Point(row, col), name)
+				board.pieces.append(piece)
+		player = Player.red if array[-2] == 1 else Player.black
+		return GameState(board, player, array[-1])
+
+	def point_to_index(self, point):
+		return point.row * 9 + point.col
+
+	def shape(self):
+		return (34, 1)
